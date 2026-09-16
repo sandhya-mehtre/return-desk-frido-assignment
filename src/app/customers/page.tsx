c323430bc13a4df8db067c9e5c062f90b30ea960
sandhya-mehtre <sandhyamehtre28@gmail.com>
2026-09-16 20:25:20 +0530

    removed about and dashboard page - and added customers and reports page - to work on them when time permits later.

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -1,16 +1,16 @@
-export default function DashboardPage() {
+export default function CustomersPage() {
   console.log("into dashboard page ; to work on it later")
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="text-center px-6">
         <h1 className="text-4xl font-bold text-gray-900 mb-4">
-          Dashboard
+          Customers
         </h1>
         <p className="text-lg text-gray-600 mb-2">
-          Dashboard page is coming soon!
+        Customers page is coming soon!
         </p>
         <p className="text-sm text-gray-500">
-          We’re working on it and will be adding the dashboard features soon.
+          We’re working on it and will be adding the Customers features soon.
         </p>
       </div>
     </main>
